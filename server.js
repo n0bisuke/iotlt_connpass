8f58b01122948158422acebde26443b7106d1af9
のびすけ <rsksound@gmail.com>
2017-08-31 02:02:06 +0900

    update

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const io = require('socket.io-client');
 const action = require('./action');
 const socket = io('mysocket.azurewebsites.net');
 
+console.log(`startup`);
+
 socket.on('connect', () => {
     socket.on('update_connpass', (msg) => {
         console.log(`message: ${msg}`);
-        console.log('start...');
+        console.log('connecting connpass...');
         action();
     });
 });
