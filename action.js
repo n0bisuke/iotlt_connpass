529479676ef081b69da86185f6e0ddd7e252c984
のびすけ <rsksound@gmail.com>
2017-08-30 18:44:43 +0900

    update

--- action.js
+++ action.js
@@ -5,11 +5,21 @@ const devices = require('puppeteer/DeviceDescriptors');
 const iPhone = devices['iPhone 6'];
 const contetRewrite = require('./libs/rewrite');
 
-const CONFIG = require('./settings/config');
+const init = () => {
+    let config = {};
+    config.USER_ID = process.env.CONNPASS_USER || require('./settings/config').USER_ID;
+    config.PASS = process.env.CONNPASS_PASS || require('./settings/config').PASS;
+    config.SS_ID = process.env.CONNPASS_SS_ID || require('./settings/config').SS_ID;
+    config.SS_RANGE = process.env.CONNPASS_SS_RANGE || require('./settings/config').SS_RANGE;
+    config.EVENT_ID = process.env.CONNPASS_EVENT_ID || require('./settings/config').EVENT_ID;
+    return config;
+}
+
+const CONFIG = init();
 const BASEURL = `https://connpass.com/login/?next=https%3A//connpass.com/event`;
 const EVENT_URL = `${BASEURL}/${CONFIG.EVENT_ID}/edit/basic`;
 
-(async () => {
+module.exports = async () => {
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
     await page.emulate(iPhone);
@@ -41,4 +51,4 @@ const EVENT_URL = `${BASEURL}/${CONFIG.EVENT_ID}/edit/basic`;
     await page.waitFor(1000);
     await page.screenshot({path: 'example.png'});
     browser.close();
-})();
+};
--- server.js
+++ server.js
@@ -2,10 +2,14 @@
 
 const fs = require('fs');
 const express = require('express');
+const action = require('./action');
 const app = express();
 const PORT = process.env.PORT || 3000;
 // app.use(express.static(__dirname));
 
-app.get('/', (req, res) => res.send(process.version));
+app.get('/', (req, res) => {
+    res.send(process.version);
+    action();
+});
  
 app.listen(PORT);
